46e2bbae177e642a04cdd380ce36ebb70718ef94
Will Holmes <willholmes97@icloud.com>
2018-06-12 23:43:44 +0100

    Initial Commit

---
+++ JournalMe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JournalMe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
